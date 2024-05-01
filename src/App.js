27b0fa51efd02f7347d1dc138da8e42b8565df64
ang <angsc0605@gmail.com>
2024-05-01 17:54:15 +0800

    100% html

--- src/App.js
+++ src/App.js
@@ -6,10 +6,17 @@ import Specials from './component/Specials.js'
 import Testimonials from './component/Testimonials.js'
 import AboutUs from './component/AboutUs.js'
 import { ChakraProvider } from '@chakra-ui/react';
+import ogimage from "./image/restauranfood.jpg"
 
 function App() {
   return (
     <ChakraProvider>
+    <head>
+    <meta name='description' content='Restaurant'></meta>
+    <meta name="og:title" content="LittleLemon"/>
+    <meta name="og:description" content="Healthy Life Style"/>
+    <meta name="og:image" content={ogimage}/>
+    </head>
     <main>
         <Headers/>
         <Main/>
