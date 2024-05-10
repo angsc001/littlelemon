2085c37e6bef3b3eabf6c41b08ce2a15d1925ce0
ang <angsc0605@gmail.com>
2024-05-10 18:53:26 +0800

    react 51%

--- src/App.test.js
+++ src/App.test.js
@@ -22,13 +22,6 @@ describe('navigates to the correct route', () => {
     );
       fireEvent.click(screen.getByText('Reserve a Table'));
       expect(screen.getByText('Choose date')).toBeInTheDocument();
-      const inputElement = screen.getByTestId('date-input');
-      const initializeTimes  = screen.getByTestId('time-input');
-      expect(initializeTimes.value).toBe('10:00 AM');
-      fireEvent.change(inputElement, { target: { value: '2024-05-10' } });
-      expect(inputElement.value).toBe('2024-05-10');
-      const updateTimes   = screen.getByTestId('time-input');
-      expect(updateTimes .value).toBe('10:30 AM')
    });
 
    it('Check initial times', () => {
